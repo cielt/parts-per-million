66f9e5d814a49cec55c8bf52576c5ad694cb57ad
leslie tu <chiayin.l.tu@gmail.com>
2023-01-24 18:51:00 -0500

    [PPM] tweak styling of home page grid feed items * make entire feed item clickable link

--- template-parts/feed/post-item-feed.php
+++ template-parts/feed/post-item-feed.php
@@ -1,6 +1,6 @@
 <li class="feed-item" id="post-<?php the_ID(); ?>" data-rel="post-feed-item">
-	<div class="feed-item-content">
-		<a href="<?php the_permalink(); ?>" class="story-thumbnail feed-item-thumbnail-link">
+	<a class="feed-item-content" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+		<span class="story-thumbnail feed-item-thumbnail-link">
 		<?php if (get_the_post_thumbnail(null, "thumbnail", "")): ?>
 					 <?php echo the_post_thumbnail(
         [320, 240],
@@ -10,7 +10,7 @@
 		<?php if (get_field("medium")): ?>
 		<span class="post-medium"><?php echo get_field("medium"); ?></span>
   <?php endif; ?>
-		</a>
+		</span>
 	<div class="story-info">
 		<div class="contributor-info">
 			<?php if (get_field("contributor_image")):
@@ -28,9 +28,9 @@
 				<?php endif; ?>
 		</div>
     <h3 class="feed-story-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+			<?php the_title(); ?>
 		</h3>
     <!-- intro -->
 	</div><!-- /.story-info  -->
-	</div>
+	</a>
 </li>
